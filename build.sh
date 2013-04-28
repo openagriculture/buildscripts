da9759906ad4befab37e89e5ad05fecf3b4b4848
Shivdas Gujare <shivdas.gujare@gmail.com>
2013-04-28 22:32:32 +0530

    Added copyright and License details

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+# Build Enviroment Setup Script
+#
+# Copyright (C) 2011,2012 Freescale Semiconductor
+# Copyright (C) 2013 [ Shivdas Gujare ] shivdas.gujare@gmail.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
 datetime=$(date +%d%m%y-%I%M)
 
 install_repo() {
